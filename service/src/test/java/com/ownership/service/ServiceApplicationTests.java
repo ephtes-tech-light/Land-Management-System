048af0263221a8063119a3f1d433c98edb01621d
ephtes <ephtestemp@gmail.com>
2026-02-03 11:00:04 +0300

    ownership service initialized

---
+++ service/src/test/java/com/ownership/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ownership.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
